e9564c77aad7c78defbf2478974ccfa3755ddd1e
Hsin-Chien Chang <kingchang33@gmail.com>
2020-08-08 01:07:53 +0800

    Add vets Controller & vets/index.html !!!

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets/"})
     public String listVets(){
         return "vets/index";
     }
